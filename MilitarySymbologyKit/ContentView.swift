d1fedf40a7a4a4ee0de4d614f85f8438ada4b96c
sergpryimachuk <serg@pryimach.uk>
2023-11-10 11:13:31 +0100

    Initial Commit

---
+++ MilitarySymbologyKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  Created with ♥ by Serhii Pryimachuk on 10.11.2023.
+//  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MilitarySymbologyKit/MilitarySymbologyKitApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created with ♥ by Serhii Pryimachuk on 10.11.2023.
+//  
+
+import SwiftUI
+
+@main
+struct MilitarySymbologyKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
