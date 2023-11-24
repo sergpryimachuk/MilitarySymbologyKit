3706851edc4e7ed0ccb1a49bf9508ceb4f006e95
sergpryimachuk <serg@pryimach.uk>
2023-11-24 16:47:27 +0100

    UPD.

---
+++ MilitarySymbologyKit/Model/Entity/EntityTypes/NamedHeadquartersEntityType.swift
@@ -0,0 +1,33 @@
+//
+//  NamedHeadquartersEntityType.swift
+//  MilitarySymbologyKit
+//
+//  Created by Ярослав Шерстюк on 24/11/2023.
+//
+
+import Foundation
+
+enum NamedHeadquartersEntityType: String, CaseIterable, Identifiable {
+    case alliedCommandEuropeRapidReactionCorps = "01"
+    case alliedCommandOperations = "02"
+    case internationalSecurityAssistanceForce = "03"
+    case multinational = "04"
+    
+    var id: String { rawValue }
+    
+    var name: String {
+        switch self {
+        case .alliedCommandEuropeRapidReactionCorps:
+            return String(localized: "Allied Command Europe Rapid Reaction Corps (ARRC)", comment: "Named Headquarters Entity Type")
+            
+        case .alliedCommandOperations:
+            return String(localized: "Allied Command Operations", comment: "Named Headquarters Entity Type")
+            
+        case .internationalSecurityAssistanceForce:
+            return String(localized: "International Security Assistance Force (ISAF)", comment: "Named Headquarters Entity Type")
+            
+        case .multinational:
+            return String(localized: "Multinational (MN)", comment: "Named Headquarters Entity Type")
+        }
+    }
+}
