264b565f1de952b16a1428a63853749519a74c96
sergpryimachuk <serg@pryimach.uk>
2023-12-03 18:25:58 +0100

    Change version.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MilitarySymbologyKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16), .macOS(.v13), .visionOS(.v1)],
+    platforms: [.iOS(.v17), .macOS(.v14), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
