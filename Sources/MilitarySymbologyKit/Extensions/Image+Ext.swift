5696eb10e276d877b1f36373decf3586100997cb
sergpryimachuk <serg@pryimach.uk>
2023-12-04 19:41:25 +0100

    Delete.

--- Sources/MilitarySymbologyKit/Extensions/Image+Ext.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Created with ♥ by Serhii Pryimachuk on 03.12.2023.
-//  
-
-import SwiftUI
-
-public extension Image {
-    static let unknownMilitarySymbol = Image(systemName: "questionmark.diamond.fill")
-}
