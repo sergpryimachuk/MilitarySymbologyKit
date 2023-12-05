69e86a63fa79cfa8be6a11d82b359445d13eb028
sergpryimachuk <serg@pryimach.uk>
2023-12-05 10:48:48 +0100

    Try view.

--- Sources/MilitarySymbologyKit/MilitarySymbolSelector.swift
+++ Sources/MilitarySymbologyKit/MilitarySymbolSelector.swift
@@ -1,6 +1,6 @@
 //
 //  Created with ♥ by Serhii Pryimachuk on 04.12.2023.
-//  
+//
 
 import SwiftUI
 
@@ -12,13 +12,12 @@ public struct MilitarySymbolSelector: View {
         List {
             Section {
                 NavigationLink {
-                    
+                    StatusSelector(symbol: $symbol)
                 } label: {
-                    
+                    Text("Select status", bundle: .module)
                 }
-
             } header: {
-                Text("Context")
+                Text("Context", bundle: .module)
             }
         }
     }
@@ -26,4 +25,44 @@ public struct MilitarySymbolSelector: View {
 
 #Preview {
     MilitarySymbolSelector(symbol: .constant(.init()))
+}
+
+public struct StatusSelector: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @Binding var symbol: MilitarySymbol
+    
+    public var body: some View {
+        List(Status.allCases) { status in
+            let illustrationSymbol = MilitarySymbol(
+                context: symbol.context,
+                standardIdentity: symbol.standardIdentity,
+                dimention: symbol.dimention,
+                status: status,
+                hqtfd: symbol.hqtfd,
+                amplifier: symbol.amplifier,
+                descriptor: symbol.descriptor,
+                entity: symbol.entity,
+                entityType: symbol.entityType,
+                entitySubtype: symbol.entitySubtype,
+                isCivilian: symbol.isCivilian,
+                isAlternateStatusAmplifiers: symbol.isAlternateStatusAmplifiers)
+            
+            Button {
+                symbol.status = status
+                dismiss()
+            } label: {
+                LabeledContent {
+                    Text(status.name)
+                } label: {
+                    illustrationSymbol.makeView(size: 50)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MilitarySymbolPicker(symbol: .constant(.init()))
 }
